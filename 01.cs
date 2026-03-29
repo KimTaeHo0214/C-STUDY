2819ddb13b8fefe6dd2c9037fcd9738c2e66e16b
KimTaeHo0214 <19w8wi2siww@gmail.com>
2026-03-29 21:48:25 +0900

    Create 01.cs

---
+++ 01.cs
@@ -0,0 +1,30 @@
+/*class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello World!");
+    }
+}
+namespace [네임 스페이스 이름]
+{
+    class [클래스 이름1]
+    {
+    }
+
+    class [클래스 이름2]
+    {
+    }
+}*/
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
